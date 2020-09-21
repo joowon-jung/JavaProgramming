ddd2bb70a5c6902d7ad96f04cd7e7e9b1d18524f
Joowon Jung <jjoowon.jung@gmail.com>
2020-09-21 22:40:30 +0900

    09-21

---
+++ src/jp02/part05/OuterClass01.java
@@ -0,0 +1,13 @@
+package jp02.part05;
+
+/*
+ * 	
+ * 	<< Inner Member Class >>
+ *  1. innerMemberClass 는 class 를 구성하는 Field, method 와 같이 Class가 구성하는
+ *    Member 가 될 수 있다.
+ *  2. 사용이유는 : OuterClass와 밀접한 관련
+ *  3. 컴파일 후 innerClass는 어떤 형태의 class가 되는지 확인 (~~~ & ~~~. class)
+ */
+public class OuterClass01 {
+
+}
