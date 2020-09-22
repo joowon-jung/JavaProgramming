2f41fcea2cefbd58932b9725e5e4218fee5a22f0
Joowon Jung <jjoowon.jung@gmail.com>
2020-09-22 21:38:11 +0900

    20-09-22

---
+++ src/jp02/part05/AnonymousInnerClass.java
@@ -0,0 +1,66 @@
+package jp02.part05;
+
+/*
+ * 
+ * 		<< Anonymous Inner Class 의 이해 >>
+ * 		1. class 의 이름이 없는 독특한 class
+ */
+
+class A {
+	///Field
+	///Constructor
+	///Method
+	public void abc(String message) {
+		System.out.println("::"+this.getClass().getName()+" start.. ");
+		System.out.println(":: abc() " + message);
+		System.out.println("::"+this.getClass().getName()+" end.. ");
+	}
+}
+
+
+public class AnonymousInnerClass {
+	
+	///Main Method
+	public static void main(String[] args) {
+		
+		//1. 인스턴스 생성, 식별성으로.. Method 접근
+		System.out.println("1.====================");
+		A a = new A();
+		a.abc("Hello");
+		
+		//2. 인스턴스 생성 후 바로 Method 접근
+		System.out.println("2.====================");
+		new A().abc("Hello");
+		
+//		 ====== 아래의 코드를 이해 ========
+//		  public class B extends A {
+//		  	public void def(String message){
+//		  	  System.out.println(":: Hi " + message);
+//		  	}
+//		  }
+//		   new B().abc("Hello");
+//		   new B().def("Hello");
+//		  ===============================
+		
+		//3. 위의 코드를 이해한다면, 아래의 코드는..
+		//==> Anonymous (익명, 불명 : 이름이 없는.. )
+		//==> 위의 소스에서 public class B extends 를 지우면...
+		System.out.println("3.====================");
+		new A() {
+			public void def(String message) {
+				System.out.println("::"+this.getClass().getName()+" start.. ");
+				System.out.println(":: def() " + message);
+				System.out.println("::"+this.getClass().getName()+" end.. ");
+			}
+		}.abc("Hello");
+		
+		System.out.println("4.====================");
+		new A() {
+			public void def(String message) {
+				System.out.println("::"+this.getClass().getName()+" start.. ");
+				System.out.println(":: def() " + message);
+				System.out.println("::"+this.getClass().getName()+" end.. ");
+			}
+		}.def("Hello");
+	}
+}
---
+++ src/jp02/part05/OuterClass02.java
@@ -0,0 +1,55 @@
+package jp02.part05;
+
+/*
+ * 	
+ * 	<< Inner local Class 의 이해 >>
+ *  1. inner localClass 는 method 와 내부의 local variable 과 같은 level
+ *  2. 컴파일 후 innerClasss는 어떤 형태의 class가 되는 지 확인할 것 (~~~ $1~~~.class)
+ */
+public class OuterClass02 {
+
+	///Field
+	private String outer = "OuterClass 의 Field";
+	
+	///Constructor
+	public OuterClass02() {
+	}
+	
+	///Method
+	public void outerMethod() {
+		
+		System.out.println("::"+this.getClass().getName()+" start.. ");
+	
+		final String localVariable = "OuterClass 내의 outerMethod 의 localVariable";
+		
+		///Inner local Class
+		class InnerLocalClass {
+			///Field
+			private String inner = "innerClass 의 Field";
+			//Constructor
+			public InnerLocalClass() {
+			}
+			///Method
+			public void innerMethod() {
+				System.out.println("::"+this.getClass().getName()+" start.. ");
+				//innerClass에서 OuterClass로 접근하기 
+				System.out.println(localVariable);
+				System.out.println(outer);
+				System.out.println(inner);
+				System.out.println("::"+this.getClass().getName()+" end.. ");
+			}
+		}
+		
+		System.out.println("::"+this.getClass().getName()+" start.. ");
+		
+		//inner local class 를 둘러싼 method에서 인스턴스 생성 가능
+		InnerLocalClass ilc = new InnerLocalClass();
+		ilc.innerMethod();
+
+	}
+	
+	///main method
+	public static void main(String[] args) {
+		new OuterClass02().outerMethod();
+	}
+}
